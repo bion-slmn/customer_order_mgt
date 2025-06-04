499fe0a8c0dfea5ef876a54a7b943dc9508750a6
bion-slmn <bionsol25@gmail.com>
2025-06-04 12:46:56 +0300

    updating the requirements

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,29 @@
-# Dockerfile
 FROM python:3.11-slim
 
+# Create non-root user
+RUN useradd -m -r appuser
+
+# Set working directory
 WORKDIR /app
 
+# Copy requirements and install
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
-
-COPY . .
-
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-
-ENV PORT=8080
-
-ENTRYPOINT ["/entrypoint.sh"]
+RUN pip install psycopg2-binary
 
 
+# Copy app files and set ownership
+COPY --chown=appuser:appuser . .
+
+
+# Environment variables
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV PORT=8000
+
+# Switch to non-root user
+USER appuser
+
+# Default command
+CMD ["gunicorn", "customer_order.wsgi:application", "--bind", "0.0.0.0:8000"]
 
--- customer_order/settings/development.py
+++ customer_order/settings/development.py
@@ -1,21 +1,30 @@
 
 from pathlib import Path
 from .base import *
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DATABASE_ENGINE', 'sqlite3')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'polls'),
+         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432),
+     }
+ }
 
 SECRET_KEY = 'django-insecure-=%1@5%v7v^zgbafou-i6o180+8%s=e7rj($5tw93ma(m(=t6@m'
 
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['my-python-app-1018047469031.europe-southwest1.run.app']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","localhost").split(",")
 
